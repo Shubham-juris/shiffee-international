7cbaee1f2b43fb906df8f70112934642fed31a2d
Roshan Singh <roshan.juris@gmail.com>
2025-08-19 18:17:33 +0530

    changes

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -9,20 +9,24 @@ import Women from "../page/Women";
 import Kids from "../page/Kids";
 import Accessories from "../page/Accessories";
 import CardComponet from "../components/common/CardComponet";
+import { ScrollToTop } from "../components/ScroolToTop";
 const AppRoute = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewArrivals />} />
-      <Route path="/sale" element={<Sale />} />
-      <Route path="/men" element={<Men />} />
-      <Route path="/women" element={<Women />} />
-      <Route path="/kids" element={<Kids />} />
-      <Route path="/accessories" element={<Accessories />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/cart" element={<CardComponet />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<NewArrivals />} />
+        <Route path="/sale" element={<Sale />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<CardComponet />} />
+      </Routes>
+    </>
   );
 };
 
